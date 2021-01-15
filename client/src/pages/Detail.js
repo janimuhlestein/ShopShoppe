0568e507e84197522fb5f6631c8cebdba3b67aa4
Jani Muhlestein <janimuhlestein@gmail.com>
2021-01-14 22:38:29 -0700

    Completed adding redux to individual pages.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useStoreContext } from '../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../utils/actions';
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
@@ -6,19 +8,22 @@ import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
+  const [ state, dispatch ] = useStoreContext();
   const { id } = useParams();
-
-  const [currentProduct, setCurrentProduct] = useState({})
-
+  const [currentProduct, setCurrentProduct] = useState({});
   const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { products } = state;
 
-  const products = data?.products || [];
-
-  useEffect(() => {
+  useEffect(()=>{
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
     }
-  }, [products, id]);
+  }, [products, data, dispatch, id]);
 
   return (
     <>
